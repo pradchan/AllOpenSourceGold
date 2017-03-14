58890e197ae26bf41362abb319bdf1a092d19613
PRADCHAN.IN <pradeep.chandramouli@oracle.com>
2017-03-14 15:28:00 +0530

    Corrected file

--- Operations/src/updateappurl.sh
+++ Operations/src/updateappurl.sh
@@ -2,5 +2,5 @@
 cloud_domain=$1
 ACCS_DATACENTER=$2
 
-ms_url="https://employeesservice-${cloud_domain}.apaas.${ACCS_DATACENTER}.oraclecloud.com/api/""
+ms_url="https://employeesservice-${cloud_domain}.apaas.${ACCS_DATACENTER}.oraclecloud.com/api/"
 sed -i 's#MICROSERVICE_URL#'${ms_url}'#' Employees/src/main/resources/public/index.html
